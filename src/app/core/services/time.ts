b64b7e475f7a3102a0e65b47bc5fe54c3d013b80
Eldi <eldi456.rangel@gmail.com>
2026-07-03 21:24:23 -0700

    cambio en el reloj , milisegundos

--- src/app/core/services/time.ts
+++ src/app/core/services/time.ts
@@ -1,70 +1,96 @@
-import { Injectable } from '@angular/core';
+import { Injectable, OnDestroy } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
-
 
 export interface TimeState {
   hours: number;
   minutes: number;
   seconds: number;
+  milliseconds: number;
   isDemo: boolean;
+  totalSeconds: number;
 }
 
 @Injectable({
-  providedIn: 'root' 
+  providedIn: 'root'
 })
-export class TimeService {
-  private timer: any;
-  private isDemoMode = false;
-
-  // BehaviorSubject almacena la hora actual y va a todos los componentes
+export class TimeService implements OnDestroy {
   private timeSubject = new BehaviorSubject<TimeState>(this.getCurrentRealTime());
-  time$ = this.timeSubject.asObservable();
+  public time$ = this.timeSubject.asObservable();
+
+  private ticker: any;
+  private isDemoMode = false;
+  private demoBaseMs = 0;
+  private realTimeAtDemoStart = 0;
 
   constructor() {
-    this.startRealTimeClock();
+   
+    this.ticker = setInterval(() => {
+      this.tick();
+    }, 50);
   }
 
-  // Obtiene la hora real 
+  private tick() {
+    const now = Date.now();
+    
+    if (!this.isDemoMode) {
+      this.timeSubject.next(this.getCurrentRealTime());
+    } else {
+      
+      const elapsedMs = now - this.realTimeAtDemoStart;
+      let currentDemoMs = this.demoBaseMs + elapsedMs;
+
+      
+      const msInDay = 86400000;
+      currentDemoMs = currentDemoMs % msInDay;
+      if (currentDemoMs < 0) currentDemoMs += msInDay; 
+
+      this.timeSubject.next(this.formatTimeFromMs(currentDemoMs, true));
+    }
+  }
+
   private getCurrentRealTime(): TimeState {
-    const now = new Date();
+    const d = new Date();
+    const totalSecs = (d.getHours() * 3600) + (d.getMinutes() * 60) + d.getSeconds();
     return {
-      hours: now.getHours(),
-      minutes: now.getMinutes(),
-      seconds: now.getSeconds(),
-      isDemo: this.isDemoMode
+      hours: d.getHours(),
+      minutes: d.getMinutes(),
+      seconds: d.getSeconds(),
+      milliseconds: d.getMilliseconds(),
+      isDemo: false,
+      totalSeconds: totalSecs + (d.getMilliseconds() / 1000)
     };
   }
 
-  
-  private startRealTimeClock() {
-    this.timer = setInterval(() => {
-      
-      if (!this.isDemoMode) {
-        this.timeSubject.next(this.getCurrentRealTime());
-      }
-    }, 1000);
-  }
+  private formatTimeFromMs(msTotal: number, isDemo: boolean): TimeState {
+    const totalSecondsFloor = Math.floor(msTotal / 1000);
+    const hours = Math.floor(totalSecondsFloor / 3600);
+    const minutes = Math.floor((totalSecondsFloor % 3600) / 60);
+    const seconds = totalSecondsFloor % 60;
+    const milliseconds = Math.floor(msTotal % 1000);
 
-  
-  setDemoTime(totalSeconds: number) {
-    this.isDemoMode = true;
-    
-    
-    const hours = Math.floor(totalSeconds / 3600);
-    const minutes = Math.floor((totalSeconds % 3600) / 60);
-    const seconds = totalSeconds % 60;
-
-    this.timeSubject.next({
+    return {
       hours,
       minutes,
       seconds,
-      isDemo: true
-    });
+      milliseconds,
+      isDemo,
+      totalSeconds: totalSecondsFloor + (milliseconds / 1000)
+    };
   }
 
- 
+  setDemoTime(segundosTotales: number) {
+    this.isDemoMode = true;
+    this.demoBaseMs = segundosTotales * 1000;
+    this.realTimeAtDemoStart = Date.now(); 
+    this.tick(); 
+  }
+
   resetToRealTime() {
     this.isDemoMode = false;
-    this.timeSubject.next(this.getCurrentRealTime());
+    this.tick();
+  }
+
+  ngOnDestroy() {
+    if (this.ticker) clearInterval(this.ticker);
   }
 }
